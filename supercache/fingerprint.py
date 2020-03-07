c7f17366dc14de7c11ad25c3c94c398b60795d51
Peter Hunt <Peter92@users.noreply.github.com>
2020-03-07 23:17:31 +0000

    Add support for kwonlyargs

--- supercache/fingerprint.py
+++ supercache/fingerprint.py
@@ -1,32 +1,44 @@
 import inspect
 import sys
+import random
 import re
 from functools import partial
 
-try:
-    from .exceptions import UnhashableError
 
-# For local testing
-except (ImportError, SystemError):
-    import os
-    sys.path.append(os.path.normpath(__file__).rsplit(os.path.sep, 2)[0])
-    from supercache.exceptions import UnhashableError
+class HashOverride(object):
+    """Make an object with a hash that will hopefully never be hit.
+    This is for when **kwargs or kwonlyargs are input as parameters to
+    reduce the chance of collisions.
+    """
+
+    def __init__(self, key):
+        self.key = key
+
+    def __hash__(self):
+        return sys.maxsize - hash(self.key)
+
+    def __eq__(self, item):
+        return False
 
 
-def default_keys(parameters, args, kwargs):
+def default_keys(func, parameters, args, kwargs, kwonlyargs):
     """Generate the default request list."""
 
     arg_count = max(len(parameters), len(args))
-    request = list(range(arg_count))
+    keys = list(range(arg_count))
+    for kwonlyarg in kwonlyargs:
+        keys.append(kwonlyarg)
     for key in sorted(kwargs):
+        if key in kwonlyargs:
+            continue
         try:
             index = parameters.index(key)
         except ValueError:
-            request.append(key)
+            keys.append(key)
         else:
-            if index >= arg_count and index not in request[:arg_count]:
-                request.append(index)
-    return request
+            if index >= arg_count and index not in keys[:arg_count]:
+                keys.append(index)
+    return keys
 
 
 def parse_key_list(lst, parameters, args, kwargs):
@@ -53,7 +65,7 @@ def parse_key_list(lst, parameters, args, kwargs):
                     strs.add(value)
 
         # Input given as regex
-        elif isinstance(key, re.Pattern):
+        elif isinstance(key, re._pattern_type):
             keywords = []
             for kwarg in kwargs:
                 if key.search(kwarg):
@@ -75,7 +87,7 @@ def parse_key_list(lst, parameters, args, kwargs):
             else:
                 ints.add(index)
 
-        return sorted(ints) + sorted(strs)
+    return sorted(ints) + sorted(strs)
 
 
 def fingerprint(fn, keys=None, ignore=None):
@@ -93,19 +105,21 @@ def fingerprint(fn, keys=None, ignore=None):
     except AttributeError:
         argument_data = inspect.getargspec(func.fget if isinstance(func, property) else func)
         parameters = argument_data.args
+        kwonlydefaults = {}
     else:
         parameters = argument_data.args
+        kwonlydefaults = argument_data.kwonlydefaults or {}
 
     # Get parameter default values as a dict
     if argument_data.defaults is None:
         default_values = {}
     else:
         default_values = dict(zip(reversed(parameters), reversed(argument_data.defaults)))
-    default_values.update(argument_data.kwonlydefaults)
+    default_values.update(kwonlydefaults)
 
     # Generate a list of parameters to use
     if keys is None:
-        keys = default_keys(parameters, args, kwargs, kwonlyargs=kwonlyargs)
+        keys = default_keys(func, parameters, args, kwargs, kwonlydefaults)
     else:
         keys = parse_key_list(keys, parameters, args, kwargs)
 
@@ -120,6 +134,15 @@ def fingerprint(fn, keys=None, ignore=None):
             # Get the argument at the index
             try:
                 hash_list.append(args[key])
+
+                # Raise error if key also exists in kwargs
+                try:
+                    param = parameters[key]
+                except IndexError:
+                    pass
+                else:
+                    if param in kwargs:
+                        raise TypeError("{}() got multiple values for keyword arguments '{}'".format(func.__name__, parameters[key]))
 
             # It may exist in *args
             except IndexError:
@@ -142,11 +165,22 @@ def fingerprint(fn, keys=None, ignore=None):
                             hash_list.append(default_values.get(param))
 
         # It may exist in **kwargs
-        # Also hash the key here, otherwise args[0] == kwargs[0]
+        # Extra hashing is required otherwise args[0] == kwargs[0]
         else:
-            hash_list += [key, kwargs.get(key)]
+            hashable_key = '__{}__'.format(key)
+            if key in kwargs:
+                hash_list += [hashable_key, kwargs[key]]
+            else:
+                hash_list += [hashable_key, default_values.get(key)]
 
     try:
         return tuple(map(hash, hash_list))
+
+    # Raise custom exception message
     except TypeError:
-        raise UnhashableError
+        raise
+        for item in hash_list:
+            try:
+                hash(item)
+            except TypeError:
+                raise TypeError("unhashable function input type: '{}'".format(item.__class__.__name__))
